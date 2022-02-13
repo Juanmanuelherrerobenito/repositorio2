ae9c8a1c8f310264e8486863447596d253c329e0
Juanmanuelherrerobenito <91980407+Juanmanuelherrerobenito@users.noreply.github.com>
2022-02-13 13:39:00 +0100

    Add files via upload

---
+++ js/funciones2.js
@@ -0,0 +1,6 @@
+function cargarPagina()
+{
+	document.getElementsByTagName("body")[0].innerHTML="Texto en el body";
+	document.getElementsByTagName("body")[0].innerHTML="Lorem Ipsum is simply dummy text of the printing and typesetting industry. ";
+	
+}
---
+++ js/funciones3.js
@@ -0,0 +1,17 @@
+function cambiarEstado()
+{
+	/*let nombre_clase=document.getElementById("parrafo_lorem").className;
+	if (nombre_clase=="mostrado")
+		{document.getElementById("parrafo_lorem").className="oculto";}
+	else
+		{document.getElementById("parrafo_lorem").className="mostrado";}*/
+	let color=document.getElementById("parrafo_lorem").style.color=;
+	if (color=="green")
+	{
+		document.getElementById("parrafo_lorem").style.color="red";
+	}
+else
+{	
+        document.getElementById("parrafo_lorem").style.color="green";
+}
+}
